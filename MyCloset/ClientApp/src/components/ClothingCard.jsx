338b766bc18d863f2a74c69f06d5bb14e2048321
cecilysavage <savagececily@gmail.com>
2022-12-29 20:01:09 -0600

    clothing card added

---
+++ MyCloset/ClientApp/src/components/ClothingCard.jsx
@@ -0,0 +1,32 @@
+﻿import '../custom.css';
+import { Text, Stack, Separator, Image, IImageProps, ImageFit } from '@fluentui/react';
+
+
+const ClothingCard = () => {
+
+    const imageProps: IImageProps = {
+        imageFit: ImageFit.cover,
+        src: 'https://via.placeholder.com/200x200.png?text=Clothing',
+    };
+
+    return (
+        <>
+            <div className='clothing-card'>
+                <Stack>
+                    <div>
+                        <Image {...imageProps} />
+                    </div>
+                    <div>
+                        <Text variant={'large'} block> Name of Item</Text>
+                        <Text block>Description of Item</Text>
+                        <Text>#Tags #Go #Here</Text>
+                    </div>
+                    
+                </Stack>
+            </div>
+        </>
+
+    );
+};
+
+export default ClothingCard;
